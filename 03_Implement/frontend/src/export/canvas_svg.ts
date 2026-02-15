ae301de38a7a28bf90cfafa20bd7288b9d0033c2
hat47x <hat47x@gmail.com>
2026-02-15 21:19:03 +0900

    Use regex escapes instead of String.replaceAll in SVG export

--- 03_Implement/frontend/src/export/canvas_svg.ts
+++ 03_Implement/frontend/src/export/canvas_svg.ts
@@ -23,11 +23,11 @@ type ExportCanvasToSvgInput = {
 
 function escapeXml(value: string): string {
   return value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&apos;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
 }
 
 function truncateText(text: string, maxLength: number): string {
